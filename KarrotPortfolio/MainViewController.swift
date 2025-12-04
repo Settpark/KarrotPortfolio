69aaed2b58ff35d6f1ddf57d36974458d5ba110e
Settpark <park9368@naver.com>
2025-12-04 15:31:10 +0900

    init: 프로젝트 초기화

---
+++ KarrotPortfolio/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  KarrotPortfolio
+//
+//  Created by temp_name on 12/4/25.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
